b375270637c9904e3afb78684d3cfd9646782bbe
chia <chelpanov@yandex.ru>
2021-03-13 13:37:32 +0300

    new: Начальная конфигурация Ktor после создания модуля приложения

---
+++ ok-catalog-be-app-ktor/build.gradle.kts
@@ -0,0 +1,30 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+
+plugins {
+    application
+    kotlin("jvm")
+}
+
+group = rootProject.group
+version = rootProject.version
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+repositories {
+    mavenLocal()
+    jcenter()
+    maven { url = uri("https://kotlin.bintray.com/ktor") }
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-host-common:$ktorVersion")
+    implementation("io.ktor:ktor-html-builder:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+}
---
+++ ok-catalog-be-app-ktor/src/main/kotlin/ru/ok/catalog/Application.kt
@@ -0,0 +1,78 @@
+package ru.ok.catalog
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.features.*
+import io.ktor.html.*
+import kotlinx.html.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+/**
+ * Please note that you can use any other name instead of *module*.
+ * Also note that you can have more then one modules in your application.
+ * */
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+        //host("xxx")
+    }
+
+
+
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+    routing {
+        install(StatusPages) {
+            exception<AuthenticationException> { cause ->
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> { cause ->
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+    }
+    routing {
+        get("/html-dsl") {
+            call.respondHtml {
+                body {
+                    h1 { +"HTML" }
+                    ul {
+                        for (n in 1..10) {
+                            li { +"$n" }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    routing {
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/static") {
+            resources("static")
+        }
+    }
+}
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
