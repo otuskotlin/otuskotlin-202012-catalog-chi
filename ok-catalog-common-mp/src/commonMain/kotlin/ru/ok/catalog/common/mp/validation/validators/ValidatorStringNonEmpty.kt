5a2dd399826407db1fae415b56239ac761b27881
chia <chelpanov@yandex.ru>
2021-04-03 20:15:12 +0300

    chng: возможность указывать поле

--- ok-catalog-common-mp/src/commonMain/kotlin/ru/ok/catalog/common/mp/validation/validators/ValidatorStringNonEmpty.kt
+++ ok-catalog-common-mp/src/commonMain/kotlin/ru/ok/catalog/common/mp/validation/validators/ValidatorStringNonEmpty.kt
@@ -1,17 +1,21 @@
 package ru.ok.catalog.common.mp.validation.validators
 
 import ru.ok.catalog.common.mp.validation.IValidator
-import ru.ok.catalog.common.mp.validation.ValidationDefaultError
+import ru.ok.catalog.common.mp.validation.ValidationFieldError
 import ru.ok.catalog.common.mp.validation.ValidationResult
 
-class ValidatorStringNonEmpty: IValidator<String?> {
+class ValidatorStringNonEmpty(
+    private val field: String = "",
+    private val message: String = "String must not be empty"
+): IValidator<String?> {
 
     override fun validate(sample: String?): ValidationResult {
         return if (sample.isNullOrBlank()) {
             ValidationResult(
                 errors = listOf(
-                    ValidationDefaultError(
-                        message = "String must not be empty",
+                    ValidationFieldError(
+                        field = field,
+                        message = message,
                     )
                 )
             )
